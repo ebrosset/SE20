34e5ed55e91614e528e9b4d35cc71480a0e0406c
ebrosset <74173436+ebrosset@users.noreply.github.com>
2020-11-20 11:13:34 +0100

    Update Sample.kt

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,18 +1,20 @@
 import kotlin.math.*
+import java.math.*
 
-fun isPrime(x:Long): Boolean{
-    if(x==1L) return false
+fun isPrime(x:BigInteger): Boolean{
+    if(x==BigInteger.valueOf(1L)) return false
 
-    for (j in 2L..x / 2) {
-            if (x % j == 0L) {
+    for (j in 2L..(sqrt(x.toDouble()).toLong())) {
+            if ((x.remainder(BigInteger.valueOf(j))).toInt() == 0){
                 return false
             }
     }
     return true
 }
+
 fun main() {
-    for (i in 1L..20L) {
-        var n = (10.0.pow(i.toDouble()) / 9).toLong()
-        println("%d %b".format(n, isPrime(n)))
+    for (i in 1..20) {
+        var n = BigInteger.valueOf(10).pow(i) / BigInteger.valueOf(9)
+        println("%s %b".format( n.toString(), isPrime(n)))
     }
 }
